7e46126ced8236daedd1d3c1696cc6dbb98d8cf6
Daniel Gregson <dpgregson@hotmail.com>
2022-10-21 17:55:53 +0100

    chore: added console project to run the library

---
+++ src/dg.kata.toyrobot.console/Program.cs
@@ -0,0 +1,8 @@
+﻿using dg.kata.toyrobot;
+using dg.kata.toyrobot.Services;
+
+const string filePath = "./Resources/InstructionFiles/Example.txt";
+
+var orchestrator = new InstructionOrchestrator(new InstructionParser(), filePath);
+
+orchestrator.RunInstructions();
